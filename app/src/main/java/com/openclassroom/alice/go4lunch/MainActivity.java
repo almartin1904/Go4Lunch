693223a712819af81aa2ad4f73a1ca104bb1c8b6
alice <alice.martinez.28@gmail.com>
2019-01-03 18:08:37 +0100

    Add Facebook and Google connexion

--- app/src/main/java/com/openclassroom/alice/go4lunch/MainActivity.java
+++ app/src/main/java/com/openclassroom/alice/go4lunch/MainActivity.java
@@ -3,11 +3,32 @@ package com.openclassroom.alice.go4lunch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.startSignInActivity();
+    }
+
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList( new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),// SUPPORT GOOGLE
+                                            new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build())) //SUPPORT FACEBOOK
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
     }
 }
